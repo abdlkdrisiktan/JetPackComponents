c0777489f2b256b05336ae7654230641e688d2ef
Abdulkadir Işıktan <aisiktan@havelsan.com.tr>
2021-09-27 11:14:35 +0300

    Word.kt created.

--- app/src/main/java/com/abdlkdr/sqlitesample/room/Word.kt
+++ app/src/main/java/com/abdlkdr/sqlitesample/room/Word.kt
@@ -1,10 +1,14 @@
 package com.abdlkdr.sqlitesample.room
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 /**
  * Created by aisiktan on 24,September,2021
  */
 @Entity(tableName = "word_table")
-data class Word(val word: String) {
+data class Word(val idValue : Int ,  val wordValue: String) {
+    @PrimaryKey(autoGenerate = true) val id : Int = idValue
+    @ColumnInfo(name = "word") val word : String = wordValue
 }
