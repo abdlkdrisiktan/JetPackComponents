e2b877a8451ac55e626e799ad4b5ebae18585865
Abdulkadir Işıktan <aisiktan@havelsan.com.tr>
2021-09-30 14:42:20 +0300

    Move id from constructor

--- app/src/main/java/com/abdlkdr/sqlitesample/room/Word.kt
+++ app/src/main/java/com/abdlkdr/sqlitesample/room/Word.kt
@@ -10,6 +10,7 @@ import java.io.Serializable
  */
 @Entity(tableName = "word_table")
 data class Word (
-    @PrimaryKey(autoGenerate = true) var id: Int = 0,
     @ColumnInfo(name = "word") val word: String
-) : Serializable {}
+) : Serializable {
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+}
